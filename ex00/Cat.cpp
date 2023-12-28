f3f55f6be30a411d8b3a23ba5d1805d539d3287e
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2023-12-28 21:36:21 +0100

    i add constructer and descructer in class cat

---
+++ ex00/Cat.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 19:47:26 by ahajji            #+#    #+#             */
+/*   Updated: 2023/12/28 21:24:02 by ahajji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+    std::cout << "call constructer form class Cat" << std::endl;
+}
+
+Cat::~Cat()
+{
+    std::cout << "call destructer from claas Cat" << std::endl;
+}
